72e21927c64a26d32149f6a07d169f9eab55023d
blackMilk123 <349083006@qq.com>
2020-06-07 21:23:37 +0800

    提交学习部分

--- security-parent/security-datasource/src/main/java/com/blackmilk/config/SecurityConfig.java
+++ security-parent/security-datasource/src/main/java/com/blackmilk/config/SecurityConfig.java
@@ -1,30 +1,45 @@
 package com.blackmilk.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
     @Bean
     PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
+
+    @Bean
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("admin").roles("admin")
-                .and()
-                .withUser("user").password("user").roles("user");
+    protected UserDetailsService userDetailsService() {
+        JdbcUserDetailsManager  manager = new JdbcUserDetailsManager (dataSource);
+
+       if (!manager.userExists("admin") ){
+           manager.createUser(User.withUsername("admin").password("admin").roles("admin").build());
+       }
+        if (!manager.userExists("user") ){
+            manager.createUser(User.withUsername("user").password("user").roles("user").build());
+        }
+        return manager;
+
     }
 
     @Override
